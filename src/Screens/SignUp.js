886479d083516c36b881755ef495912a5ce43669
brianobmalay <nikadas157@comcast.net>
2020-09-21 19:14:28 -0500

    handle signup inputs

--- src/Screens/SignUp.js
+++ src/Screens/SignUp.js
@@ -1,26 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 
-function handleSubmit(e) {
-	alert("hey yo");
-}
-
 function SignUp() {
+	const [emailData, setEmailData] = useState({
+		email: "",
+	});
+
+	const [passwordData, setPasswordData] = useState({
+		password: "",
+	});
+
+	const [usernameData, setUsernameData] = useState({
+		username: "",
+	});
+
+	function emailChange(e) {
+		setEmailData({ email: e.target.value });
+	}
+	function usernameChange(e) {
+		setUsernameData({ username: e.target.value });
+	}
+	function passwordChange(e) {
+		setPasswordData({ password: e.target.value });
+	}
+
+	function handleSubmit(e) {
+		e.preventDefault();
+
+		console.log(emailData.email, passwordData.password, usernameData.username);
+	}
+
 	return (
 		<div className="container center_div">
 			<Form onSubmit={handleSubmit}>
 				<Form.Group>
 					<Form.Group>
 						<Form.Label>Email</Form.Label>
-						<Form.Control type="email" placeholder="Enter email" />
+						<Form.Control
+							type="email"
+							name="email"
+							placeholder="Enter email"
+							value={EventTarget.value}
+							onChange={emailChange}
+						/>
 					</Form.Group>
 					<Form.Label>Username</Form.Label>
-					<Form.Control type="username" placeholder="Enter username" />
+					<Form.Control
+						type="username"
+						name="username"
+						placeholder="Enter username"
+						value={EventTarget.value}
+						onChange={usernameChange}
+					/>
 				</Form.Group>
 
 				<Form.Group>
 					<Form.Label>Password</Form.Label>
-					<Form.Control type="password" placeholder="Enter password" />
+					<Form.Control
+						type="password"
+						name="password"
+						placeholder="Enter password"
+						value={EventTarget.value}
+						onChange={passwordChange}
+					/>
 				</Form.Group>
 
 				<Button type="submit">Register</Button>
