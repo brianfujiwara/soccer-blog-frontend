9c7efe0522bae41316e5e9cf66b4a6711d33006c
brianobmalay <nikadas157@comcast.net>
2020-11-07 12:39:32 -0600

    login screen change

--- src/Screens/Login.js
+++ src/Screens/Login.js
@@ -1,24 +1,22 @@
 import React from "react";
-import { Card, Form, Button } from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
 
 function Login() {
 	return (
 		<div className="container center_div mt-3">
-			<Card className="card card-container">
-				<Form>
-					<Form.Group>
-						<Form.Label>Username</Form.Label>
-						<Form.Control type="username" placeholder="Enter username" />
-					</Form.Group>
+			<Form>
+				<Form.Group>
+					<Form.Label>Username</Form.Label>
+					<Form.Control type="username" placeholder="Enter username" />
+				</Form.Group>
 
-					<Form.Group>
-						<Form.Label>Password</Form.Label>
-						<Form.Control type="password" placeholder="Enter password" />
-					</Form.Group>
+				<Form.Group>
+					<Form.Label>Password</Form.Label>
+					<Form.Control type="password" placeholder="Enter password" />
+				</Form.Group>
 
-					<Button type="submit">Login</Button>
-				</Form>
-			</Card>
+				<Button type="submit">Login</Button>
+			</Form>
 		</div>
 	);
 }
