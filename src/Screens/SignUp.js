cf8ab790cf9803c3f1bac9520831d766d2c0cff2
brianobmalay <nikadas157@comcast.net>
2020-12-19 21:03:56 -0600

    login screen

--- src/Screens/SignUp.js
+++ src/Screens/SignUp.js
@@ -10,7 +10,7 @@ function SignUp() {
 		username: "",
 		email: "",
 		password: "",
-		loggedIn: localStorage.getItem("loggedIn") || "false",
+		//loggedIn: localStorage.getItem("loggedIn") || "false",
 	});
 
 	function handleSubmit(e) {
@@ -24,7 +24,7 @@ function SignUp() {
 			.post(URL, { email, password, username })
 			.then((Response) => {
 				//setUser({ ...user, loggedIn: true });
-				localStorage.setItem("loggedIn", "true");
+				//localStorage.setItem("loggedIn", "true");
 				if (Response.data.accessToken) {
 					localStorage.setItem("user", JSON.stringify(Response.data));
 					console.log(Response.data);
@@ -33,51 +33,47 @@ function SignUp() {
 			.catch(setUser({ ...user, loggedIn: false }));
 	}
 
-	if (user.loggedIn === "true") {
-		return <div>Logged in</div>;
-	} else {
-		return (
-			<div className="container center_div">
-				<Form onSubmit={handleSubmit}>
-					<div>
+	return (
+		<div className="container center_div">
+			<Form onSubmit={handleSubmit}>
+				<div>
+					<Form.Group>
 						<Form.Group>
-							<Form.Group>
-								<Form.Label>Email</Form.Label>
-								<Form.Control
-									type="email"
-									name="email"
-									placeholder="Enter email"
-									onChange={(e) => setUser({ ...user, email: e.target.value })}
-									value={user.email}
-								/>
-							</Form.Group>
-							<Form.Label>Username</Form.Label>
+							<Form.Label>Email</Form.Label>
 							<Form.Control
-								type="username"
-								name="username"
-								placeholder="Enter username"
-								onChange={(e) => setUser({ ...user, username: e.target.value })}
-								value={user.username}
+								type="email"
+								name="email"
+								placeholder="Enter email"
+								onChange={(e) => setUser({ ...user, email: e.target.value })}
+								value={user.email}
 							/>
 						</Form.Group>
+						<Form.Label>Username</Form.Label>
+						<Form.Control
+							type="username"
+							name="username"
+							placeholder="Enter username"
+							onChange={(e) => setUser({ ...user, username: e.target.value })}
+							value={user.username}
+						/>
+					</Form.Group>
 
-						<Form.Group>
-							<Form.Label>Password</Form.Label>
-							<Form.Control
-								type="password"
-								name="password"
-								placeholder="Enter password"
-								onChange={(e) => setUser({ ...user, password: e.target.value })}
-								value={user.password}
-							/>
-						</Form.Group>
-					</div>
+					<Form.Group>
+						<Form.Label>Password</Form.Label>
+						<Form.Control
+							type="password"
+							name="password"
+							placeholder="Enter password"
+							onChange={(e) => setUser({ ...user, password: e.target.value })}
+							value={user.password}
+						/>
+					</Form.Group>
+				</div>
 
-					<Button type="submit">Register</Button>
-				</Form>
-			</div>
-		);
-	}
+				<Button type="submit">Register</Button>
+			</Form>
+		</div>
+	);
 }
 
 export default SignUp;
